357e14ea71abd70bb5aff59951222b908ef05443
tyler58546 <tyler@kk2.com>
2018-07-10 14:33:39 -0700

    Initial Commit

---
+++ Annoyer Mobile 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Annoyer Mobile 2
+//
+//  Created by Tyler Knox on 7/10/18.
+//  Copyright © 2018 tyler58546. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
